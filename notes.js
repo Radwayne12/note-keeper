f36c981455252dc34db2d0fc48ad7bb70baeb0cb
Raduan <raduan3000@gmail.com>
2024-05-21 00:08:45 +0200

    setting up localstorage

--- notes.js
+++ notes.js
@@ -1,1 +1,7 @@
-let notes = [];
+let notesArray = localStorage.getItem("notes");
+notesArray = JSON.parse(notesArray);
+
+//initialize notesArray
+if (!notesArray) {
+    notesArray = [{"note":"", "dueDate":""}, {"note":"default note", "dueDate":"21-05-24"}];
+  }
