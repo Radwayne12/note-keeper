c906e9e2cc10ce3478131d59ebfc0fd7206960d4
Raduan <rauan3002@gmail.com>
2024-05-22 22:10:03 +0200

    populate/delete default notes

--- notes.js
+++ notes.js
@@ -5,3 +5,40 @@ notesArray = JSON.parse(notesArray);
 if (!notesArray) {
     notesArray = [{"note":"", "dueDate":""}, {"note":"default note", "dueDate":"21-05-24"}];
   }
+
+const addButton = document.getElementById("js-add-notes");
+
+function populateExistingNotes() {
+
+    let htmlNotes = '';
+
+    for (let i=1; i<notesArray.length; i++) {
+
+        htmlNotes +=`<div class="records">${notesArray[i]["note"]}</div>
+        <div class="records">${notesArray[i]["dueDate"]}</div>
+        <button class="js-delete-button">Delete</button>`
+    };
+
+    document.querySelector('.js-notes-populate').innerHTML = htmlNotes;
+
+    const deleteButtons = document.getElementsByClassName("js-delete-button");
+
+    Array.from(deleteButtons).forEach((value, index) => {
+      deleteNote(value, index + 1)
+    });
+    
+    localStorage.setItem("notes", JSON.stringify(notesArray)); //to update localstorage in future
+}
+
+populateExistingNotes();
+
+
+
+function deleteNote(value, index) {
+    value.addEventListener("click", () => {
+      notesArray.splice(index,1)
+      populateExistingNotes()
+    });
+};
+
+
